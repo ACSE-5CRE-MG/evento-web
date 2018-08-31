c64676e25d02b9ec58d6b48ab9253763751a20ba
Robson <robsonmbobbi@gmail.com>
2018-08-31 17:08:54 -0300

    Continuação...

---
+++ EventoWeb.Nucleo/Persistencia/Contexto.cs
@@ -0,0 +1,82 @@
+﻿using EventoWeb.Nucleo.Negocio.Repositorios;
+using EventoWeb.Nucleo.Persistencia.Repositorios;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Context;
+using NHibernate.Mapping.ByCode;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace EventoWeb.Nucleo.Persistencia
+{
+    public class Contexto
+    {
+        private static ISessionFactory mFabricaSession;
+
+        private static ISession SessionEmAberto
+        {
+            get
+            {
+                if (mFabricaSession == null)
+                {
+                    NHibernate.Cfg.Environment.UseReflectionOptimizer = false;
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    var mapper = new ModelMapper();
+
+                    configuration.AddAssembly("EventoWeb.Nucleo");
+                    mapper.AddMappings(Assembly.GetExecutingAssembly().GetExportedTypes());
+
+                    configuration.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+                    mFabricaSession = configuration.BuildSessionFactory();
+                }
+
+                if (CurrentSessionContext.HasBind(mFabricaSession) && !mFabricaSession.GetCurrentSession().IsOpen)
+                    CurrentSessionContext.Unbind(mFabricaSession);
+
+                if (!CurrentSessionContext.HasBind(mFabricaSession))
+                    CurrentSessionContext.Bind(mFabricaSession.OpenSession());
+
+                return mFabricaSession.GetCurrentSession();
+            }
+        }
+
+        public void IniciarTransacao()
+        {
+            SessionEmAberto.BeginTransaction();
+        }
+
+        public void SalvarTransacao()
+        {
+            var session = SessionEmAberto;
+            if (session.Transaction.IsActive)
+                session.Transaction.Commit();
+        }
+
+        public void CancelarTransacao()
+        {
+            var session = SessionEmAberto;
+            if (session.Transaction.IsActive)
+                session.Transaction.Rollback();
+        }
+
+        public void FinalizarConexao()
+        {
+            var session = SessionEmAberto;
+            session.Close();
+            session.Dispose();
+        }
+
+        public static AEventos RepositorioEventos
+        {
+            get
+            {
+                return new RepositorioEventosNH(SessionEmAberto);
+            }
+        }
+
+    }
+}
---
+++ EventoWeb.Nucleo/Persistencia/Repositorios/RepositorioEventosNH.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventoWeb.Nucleo.Negocio.Entidades;
+using EventoWeb.Nucleo.Negocio.Repositorios;
+using NHibernate;
+
+namespace EventoWeb.Nucleo.Persistencia.Repositorios
+{
+    public class RepositorioEventosNH: AEventos
+    {
+        private ISession mSessao;
+
+        public RepositorioEventosNH(ISession sessao): base(new PersistenciaNH<Evento>(sessao))
+        {
+            mSessao = sessao;
+        }
+
+        public override IList<Evento> ObterTodosEventos()
+        {
+            return mSessao.QueryOver<Evento>().List();
+        }
+
+        public override Evento ObterEventoPeloId(int id)
+        {
+            return mSessao.Get<Evento>(id);
+        }
+    }
+}
