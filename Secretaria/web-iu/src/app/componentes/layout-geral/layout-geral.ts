f024f3322f1908c70198afaebef4d3288f46031f
Robson <robsonmbobbi@gmail.com>
2019-07-08 22:50:47 -0300

    Atualização das bibliotecas do angular para a secretaria. Criação do projeto de inscrição.

--- Secretaria/web-iu/src/app/componentes/layout-geral/layout-geral.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'layout-geral',
-  styleUrls: ['./layout-geral.scss'],
-  templateUrl: './layout-geral.html',
-})
-export class LayoutGeral {
-}
