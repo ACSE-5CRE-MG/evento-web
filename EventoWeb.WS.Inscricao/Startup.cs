b58b4d2473154418ae34dff37dfca282044f54c3
Robson <robsonmbobbi@gmail.com>
2019-07-08 22:53:50 -0300

    Atualziação de bibliotecas do angular no projeto de secretaria. Criação do projeto de inscrição online

---
+++ EventoWeb.WS.Inscricao/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net;
+
+namespace EventoWeb.WS.Inscricao
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opcoes => opcoes.SerializerSettings.ContractResolver = new DefaultContractResolver());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(
+                            JsonConvert.SerializeObject(ProcessarErro(contextFeature.Error)));
+                    }
+                });
+            });
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+
+            app.UseMvc();
+        }
+
+        private static Object ProcessarErro(Exception erro)
+        {
+            if (erro == null)
+                return null;
+            else
+                return new
+                {
+                    ClasseErro = erro.GetType().Name,
+                    MensagemErro = erro.Message,
+                    ErroInterno = ProcessarErro(erro.InnerException),
+                    PilhaExecucao = erro.StackTrace
+                };
+        }
+    }
+}
--- Secretaria/EventoWeb.WS.Secretaria/Startup.cs
+++ Secretaria/EventoWeb.WS.Secretaria/Startup.cs
@@ -98,20 +98,9 @@ namespace EventoWeb.WS.Secretaria
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
-            app.UseHttpsRedirection();
             app.UseMvc();
 
             app.UseSpa(spa =>
