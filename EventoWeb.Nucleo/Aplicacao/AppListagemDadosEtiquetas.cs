d14c7f18834e2f471b3054f5915f1c6cfecc7323
Robson <robsonmbobbi@gmail.com>
2023-11-01 09:40:23 -0300

    Back-end da geraçã de etiquetas do crachá e do caderno.

---
+++ EventoWeb.Nucleo/Aplicacao/AppListagemDadosEtiquetas.cs
@@ -0,0 +1,23 @@
+﻿using EventoWeb.Nucleo.Negocio.Repositorios;
+using System.Collections.Generic;
+
+namespace EventoWeb.Nucleo.Aplicacao
+{
+    public class AppListagemDadosEtiquetas : AppBase
+    {
+        public AppListagemDadosEtiquetas(IContexto contexto) : base(contexto)
+        {
+        }
+
+        public IList<CrachaInscrito> Listar(int idEvento)
+        {
+            IList<CrachaInscrito> lista = new List<CrachaInscrito>();
+            ExecutarSeguramente(() =>
+            {
+                lista = Contexto.RepositorioInscricoes.ListarCrachasInscritosPorEvento(idEvento);
+            });
+
+            return lista;
+        }    
+    }
+}
---
+++ Secretaria/EventoWeb.WS.Secretaria/Controllers/EtiquetasController.cs
@@ -0,0 +1,46 @@
+﻿using EventoWeb.Nucleo.Aplicacao;
+using EventoWeb.Nucleo.Negocio.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EventoWeb.WS.Secretaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EtiquetasController : ControllerBase
+    {
+        private const string TIPO_CONTEUDO_PDF = "application/pdf";
+
+        private readonly AppGeracaoEtiquetaCracha mAppGeracaoCracha;
+        private readonly AppGeracaoEtiquetaCaderno mAppGeracaoCaderno;
+        private readonly AppListagemDadosEtiquetas mAppListagem;
+
+        public EtiquetasController(IContexto contexto)
+        {
+            mAppListagem = new AppListagemDadosEtiquetas(contexto);
+            mAppGeracaoCracha = new AppGeracaoEtiquetaCracha();
+            mAppGeracaoCaderno = new AppGeracaoEtiquetaCaderno();
+        }
+
+        //[Authorize("Bearer")]
+        [HttpGet("evento/{idEvento}/listagem")]
+        public IList<CrachaInscrito> Listar(int idEvento)
+        {
+            return mAppListagem.Listar(idEvento);
+        }
+
+        //[Authorize("Bearer")]
+        [HttpPut("geracao-cracha")]
+        public IActionResult GerarCracha(IList<CrachaInscrito> inscritos)
+        {
+            return File(mAppGeracaoCracha.Gerar(inscritos), TIPO_CONTEUDO_PDF);
+        }
+
+        //[Authorize("Bearer")]
+        [HttpPut("geracao-caderno")]
+        public IActionResult GerarCaderno(IList<CrachaInscrito> inscritos)
+        {
+            return File(mAppGeracaoCaderno.Gerar(inscritos), TIPO_CONTEUDO_PDF);
+        }
+    }
+}
